a43e34ae278cb8aac8904cac6fa709b244b8ee29
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-12 23:40:46 +0600

    only valid user can see his added foods. Middlware implemented on backend

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,6 +1,4 @@
 import axios from "axios";
-import useAuth from "./useAuth";
-import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
    baseURL: import.meta.env.VITE_API_URL,
@@ -8,30 +6,6 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
-   const { logOut } = useAuth();
-   const navigate = useNavigate();
-
-   // interceptors
-   axiosSecure.interceptors.response.use(
-      (res) => {
-         return res;
-      },
-      async (error) => {
-         console.log(`Error found in interceptor`, error.response);
-
-         if (error.response.status === 401 || error.response.status === 403) {
-            console.log(`Forbidden or Unauthorized. Will logout now.`);
-
-            await logOut();
-            navigate("/login");
-         }
-
-         // amra ager joto try catch lekhsi.. error catch scope
-         // e dekhte chaile ai line ta dite hobe.
-         return Promise.reject(error);
-      }
-   );
-
    return axiosSecure;
 };
 
