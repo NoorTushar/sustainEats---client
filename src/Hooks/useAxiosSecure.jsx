916814634e9aa78fbf651749350d74d0d132175d
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-13 00:05:02 +0600

    axios secure e error status message porte partesi.. but logout ta use korte partesina

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect } from "react";
 
 const axiosSecure = axios.create({
    baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +7,21 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+   useEffect(() => {
+      axiosSecure.interceptors.response.use(
+         (res) => {
+            return res;
+         },
+         (error) => {
+            console.log(`error tracked in the interceptor`, error.response);
+
+            if (error.response.status === 401 || error.response.error === 403) {
+               console.log(`Unauthorized access. Logout user.`);
+            }
+         }
+      );
+   }, []);
+
    return axiosSecure;
 };
 
