c437c429fc2e95ab2a63f7dfee881f67f249eafd
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-12 22:46:35 +0600

    completed the axios intercepter setup for unauthorized logins

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
    baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +8,30 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+   const { logOut } = useAuth();
+   const navigate = useNavigate();
+
+   // interceptors
+   axiosSecure.interceptors.response.use(
+      (res) => {
+         return res;
+      },
+      async (error) => {
+         console.log(`Error found in interceptor`, error.response);
+
+         if (error.response.status === 401 || error.response.status === 403) {
+            console.log(`Forbidden or Unauthorized. Will logout now.`);
+
+            await logOut();
+            navigate("/login");
+         }
+
+         // amra ager joto try catch lekhsi.. error catch scope
+         // e dekhte chaile ai line ta dite hobe.
+         return Promise.reject(error);
+      }
+   );
+
    return axiosSecure;
 };
 
