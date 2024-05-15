ed7fe12edcec37f4d34d511c3e8dd2d552f46684
NoorTushar <tushar.noor.khan@gmail.com>
2024-05-15 23:41:01 +0600

    fixed color of login form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -117,9 +117,9 @@ const Login = () => {
             <title>Login | SustainEats</title>
          </Helmet>
          {/* Title */}
-         <Title title="login"></Title>
+         <Title title="Login"></Title>
 
-         <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl ">
+         <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg lg:max-w-4xl ">
             <div
                className="hidden bg-cover lg:block lg:w-1/2"
                style={{
@@ -132,14 +132,14 @@ const Login = () => {
                   <img className="w-auto h-7 sm:h-8" src={logo} alt="" />
                </div>
 
-               <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
+               <p className="mt-3 text-xl text-center text-black ">
                   Welcome back!
                </p>
 
                {/* google login */}
                <button
                   onClick={handleLoginWithGoogle}
-                  className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600 w-full"
+                  className="flex items-center justify-center mt-4 text-black transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50  w-full"
                >
                   <div className="px-4 py-2">
                      <svg className="w-6 h-6" viewBox="0 0 40 40">
@@ -168,9 +168,9 @@ const Login = () => {
                </button>
 
                <div className="flex items-center justify-between mt-4">
-                  <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+                  <span className="w-1/5 border-b  lg:w-1/4"></span>
 
-                  <p className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 ">
+                  <p className="text-xs text-center text-black uppercase  ">
                      or login with email
                   </p>
 
@@ -182,7 +182,7 @@ const Login = () => {
                   {/* Email */}
                   <div className="mt-4">
                      <label
-                        className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                        className="block mb-2 text-sm font-medium text-black "
                         htmlFor="email"
                      >
                         Email Address
@@ -199,7 +199,7 @@ const Login = () => {
                               message: "Must provide a valid email address",
                            },
                         })}
-                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
                         type="email"
                         name="email"
                         id="email"
@@ -217,14 +217,14 @@ const Login = () => {
                   <div className="mt-4">
                      <div className="flex justify-between">
                         <label
-                           className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                           className="block mb-2 text-sm font-medium text-black "
                            htmlFor="loggingPassword"
                         >
                            Password
                         </label>
                         {/* <a
                      href="#"
-                     className="text-xs text-gray-500 dark:text-gray-300 hover:underline"
+                     className="text-xs text-black  hover:underline"
                   >
                      Forget Password?
                   </a> */}
@@ -239,7 +239,7 @@ const Login = () => {
                                  message: "Must provide a password.",
                               },
                            })}
-                           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                           className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
                            // toggle show/ hide password - (3)
                            type={showPassword ? "text" : "password"}
                            name="password"
@@ -278,17 +278,17 @@ const Login = () => {
                </form>
 
                <div className="flex items-center justify-between mt-4">
-                  <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                  <span className="w-1/5 border-b  md:w-1/4"></span>
                   {/* // after registration correct redirection - (7) */}
                   <Link
                      state={locationState}
                      to={"/registration"}
-                     className="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+                     className="text-xs text-black uppercase  hover:underline"
                   >
                      or register
                   </Link>
 
-                  <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                  <span className="w-1/5 border-b  md:w-1/4"></span>
                </div>
             </div>
          </div>
